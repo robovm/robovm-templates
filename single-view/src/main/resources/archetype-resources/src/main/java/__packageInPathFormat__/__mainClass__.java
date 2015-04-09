f3344d946797f000440cdd53cae76eb9580d1df2
Niklas Therning <niklas@therning.org>
2015-04-09 19:49:24 +0200

    Added new template corresponding to Xcode's Single View template which uses storyboards and asset catalogs. (fixes #9)

---
+++ single-view/src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/__mainClass__.java
@@ -0,0 +1,22 @@
+package ${package};
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+import org.robovm.apple.uikit.UIApplicationDelegateAdapter;
+import org.robovm.apple.uikit.UIApplicationLaunchOptions;
+import org.robovm.apple.uikit.UIScreen;
+import org.robovm.apple.uikit.UIWindow;
+
+public class ${mainClass} extends UIApplicationDelegateAdapter {
+
+    @Override
+    public boolean didFinishLaunching(UIApplication application, UIApplicationLaunchOptions launchOptions) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        try (NSAutoreleasePool pool = new NSAutoreleasePool()) {
+            UIApplication.main(args, null, ${mainClass}.class);
+        }
+    }
+}
