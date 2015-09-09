cd6296493bb5c68039385d1b1322b8d425a3a77e
badlogic <badlogicgames@gmail.com>
2015-09-09 22:41:21 +0200

    Made cross-platform template use Java 8 & RetroLambda on all platforms

--- cross-platform/src/main/resources/archetype-resources/android/src/main/java/__packageInPathFormat__/__mainClass__Activity.java
+++ cross-platform/src/main/resources/archetype-resources/android/src/main/java/__packageInPathFormat__/__mainClass__Activity.java
@@ -17,12 +17,9 @@ public class ${mainClass}Activity extends Activity {
         final TextView counterTextView = (TextView) findViewById(R.id.counterTextView);
         final Button counterButton = (Button) findViewById(R.id.counterButton);
 
-        counterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                counterStore.add(1);
-                counterTextView.setText("Click Nr. " + counterStore.get());
-            }
+        counterButton.setOnClickListener((view) -> {
+            counterStore.add(1);
+            counterTextView.setText("Click Nr. " + counterStore.get());
         });
     }
 }
