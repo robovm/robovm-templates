31a4689cbb781532926abec8e5f11fb617728c1c
Niklas Therning <niklas@therning.org>
2015-12-07 17:47:39 +0100

    Added tvos-single-view template. Still missing app icons and launch image. (#33)

---
+++ tvos-single-view/src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/MyViewController.java
@@ -0,0 +1,19 @@
+${package}
+
+import org.robovm.apple.uikit.UILabel;
+import org.robovm.apple.uikit.UIViewController;
+import org.robovm.objc.annotation.CustomClass;
+import org.robovm.objc.annotation.IBAction;
+import org.robovm.objc.annotation.IBOutlet;
+
+@CustomClass("MyViewController")
+public class MyViewController extends UIViewController {
+    @IBOutlet
+    private UILabel label;
+    private int clickCount;
+
+    @IBAction
+    private void clicked() {
+        label.setText("Click Nr. " + (++clickCount));
+    }
+}
